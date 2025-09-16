06136860b52db36401b538122a4ccd2e510c1cf8
Ilia Mirzaali <46376329+ramzxy@users.noreply.github.com>
2025-09-16 22:43:49 +0200

    Add project files.

---
+++ Emuchip8/Emuchip8.cpp
@@ -0,0 +1,12 @@
+﻿// Emuchip8.cpp : Defines the entry point for the application.
+//
+
+#include "Emuchip8.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
