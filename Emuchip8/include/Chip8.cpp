22b82c2d38fb616daff62518cdcf5389af0a1eb9
Ilia Mirzaali <46376329+ramzxy@users.noreply.github.com>
2025-09-18 00:01:06 +0200

    added instructoin headers and finished constructor

--- Emuchip8/include/Chip8.cpp
+++ Emuchip8/include/Chip8.cpp
@@ -4,15 +4,188 @@
 
 const unsigned int START_ADDRESS = 0x200;
 
+const unsigned int FONTSET_SIZE = 80;
+const unsigned int FONTSET_START_ADDRESS = 0x50;
+
+uint8_t fontset[FONTSET_SIZE] =
+{
+	0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+	0x20, 0x60, 0x20, 0x20, 0x70, // 1
+	0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+	0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+	0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+	0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+	0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+	0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+	0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+	0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+	0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+	0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+	0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+	0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+	0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+	0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+};
+
+Chip8::Chip8()
+	: randGen(std::chrono::system_clock::now().time_since_epoch().count()),
+		randByte(0, 255U)
+{
+	// Initialize program counter at start address of instructions
+	pc = START_ADDRESS;
+
+	// Load fontset on ROM
+	for (unsigned int i = 0; i < FONTSET_SIZE; ++i)
+	{
+		memory[FONTSET_START_ADDRESS + i] = fontset[i];
+	}
+}
+
 void Chip8::loadROM(char const* fileName) {
 	// Open the file using ifstream, turn into binray and move file pointer to eof
 	std::ifstream file(fileName, std::ios::binary);
 
 	if (file.is_open()) {
-		// Using a C++17+ approach to read the file stream into a char vector safe
+		// read the file stream into a char vector safe
 		std::vector<char> buffer(std::istreambuf_iterator<char>(file), {});
+		int size = buffer.size();
 
+		for (long i = 0; i < size; ++i) {
+			memory[START_ADDRESS + i] = buffer[i];
+		}
 
-
+		//buffer is cleaned when out of scope
 	}
+}
+
+void Chip8::OP_00E0()
+{
+}
+
+void Chip8::OP_00EE()
+{
+}
+
+void Chip8::OP_1nnn()
+{
+}
+
+void Chip8::OP_2nnn()
+{
+}
+
+void Chip8::OP_3xkk()
+{
+}
+
+void Chip8::OP_4xkk()
+{
+}
+
+void Chip8::OP_5xy0()
+{
+}
+
+void Chip8::OP_6xkk()
+{
+}
+
+void Chip8::OP_7xkk()
+{
+}
+
+void Chip8::OP_8xy0()
+{
+}
+
+void Chip8::OP_8xy1()
+{
+}
+
+void Chip8::OP_8xy2()
+{
+}
+
+void Chip8::OP_8xy3()
+{
+}
+
+void Chip8::OP_8xy4()
+{
+}
+
+void Chip8::OP_8xy6()
+{
+}
+
+void Chip8::OP_8xy7()
+{
+}
+
+void Chip8::OP_8xyE()
+{
+}
+
+void Chip8::OP_9xy0()
+{
+}
+
+void Chip8::OP_Annn()
+{
+}
+
+void Chip8::OP_Bnnn()
+{
+}
+
+void Chip8::OP_Cxkk()
+{
+}
+
+void Chip8::OP_Dxyn()
+{
+}
+
+void Chip8::OP_Ex9E()
+{
+}
+
+void Chip8::OP_ExA1()
+{
+}
+
+void Chip8::OP_Fx07()
+{
+}
+
+void Chip8::OP_Fx0A()
+{
+}
+
+void Chip8::OP_Fx15()
+{
+}
+
+void Chip8::OP_Fx18()
+{
+}
+
+void Chip8::OP_Fx1E()
+{
+}
+
+void Chip8::OP_Fx29()
+{
+}
+
+void Chip8::OP_Fx33()
+{
+}
+
+void Chip8::OP_Fx55()
+{
+}
+
+void Chip8::OP_Fx65()
+{
 }
--- Emuchip8/include/Chip8.h
+++ Emuchip8/include/Chip8.h
@@ -1,4 +1,6 @@
 #include <cstdint>
+#include <chrono>
+#include <random>
 
 class Chip8 {
 public:
@@ -14,7 +16,117 @@ public:
 	uint32_t video[64 * 32]{};
 	uint16_t opcode;
 
-	//methods
 
+
+	// General methods
+	Chip8();
 	void loadROM(char const*);
+
+	// Commands
+
+	// 00E0: CLS
+	void OP_00E0();
+
+	// 00EE: RET
+	void OP_00EE();
+
+	// 1nnn: JP addr
+	void OP_1nnn();
+
+	// 2nnn - CALL addr
+	void OP_2nnn();
+
+	// 3xkk - SE Vx, byte
+	void OP_3xkk();
+
+	// 4xkk - SNE Vx, byte
+	void OP_4xkk();
+
+	//5xy0 - SE Vx, Vy
+	void OP_5xy0();
+
+	// 6xkk - LD Vx, byte
+	void OP_6xkk();
+
+	// 7xkk - ADD Vx, byte
+	void OP_7xkk();
+
+	// 8xy0 - LD Vx, Vy
+	void OP_8xy0();
+
+	// 8xy1 - OR Vx, Vy
+	void OP_8xy1();
+
+	// 8xy2 - AND Vx, Vy
+	void OP_8xy2();
+
+	// 8xy3 - XOR Vx, Vy
+	void OP_8xy3();
+
+	// 8xy4 - ADD Vx, Vy
+	void OP_8xy4();
+
+	// 8xy5 - SUB Vx, Vy
+	void OP_8xy4();
+
+	// 8xy6 - SHR Vx
+	void OP_8xy6();
+
+	// 8xy7 - SUBN Vx, Vy
+	void OP_8xy7();
+
+	// 8xyE - SHL Vx {, Vy}
+	void OP_8xyE();
+
+	// 9xy0 - SNE Vx, Vy
+	void OP_9xy0();
+
+	// Annn - LD I, addr
+	void OP_Annn();
+
+	// Bnnn - JP V0, addr
+	void OP_Bnnn();
+
+	// Cxkk - RND Vx, byte
+	void OP_Cxkk();
+
+	// Dxyn - DRW Vx, Vy, nibble
+	void OP_Dxyn();
+
+	// Ex9E - SKP Vx
+	void OP_Ex9E();
+
+	// ExA1 - SKNP Vx
+	void OP_ExA1();
+
+	// Fx07 - LD Vx, DT
+	void OP_Fx07();
+
+	// Fx0A - LD Vx, K
+	void OP_Fx0A();
+
+	// Fx15 - LD DT, Vx
+	void OP_Fx15();
+
+	// Fx18 - LD ST, Vx
+	void OP_Fx18();
+
+	// Fx1E - ADD I, Vx
+	void OP_Fx1E();
+
+	// Fx29 - LD F, Vx
+	void OP_Fx29();
+
+	// Fx33 - LD B, Vx
+	void OP_Fx33();
+
+	// Fx55 - LD [I], Vx
+	void OP_Fx55();
+
+	// Fx65 - LD Vx, [I]
+	void OP_Fx65();
+
+private:
+	std::default_random_engine randGen;
+	std::uniform_int_distribution<uint8_t> randByte;
 };
