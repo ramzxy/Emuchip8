7abc5c20b1f041be012bfec15757a972de855aa2
Ilia Mirzaali <46376329+ramzxy@users.noreply.github.com>
2025-09-19 23:35:16 +0200

    added op code parsing

--- Emuchip8/include/Chip8.h
+++ Emuchip8/include/Chip8.h
@@ -126,7 +126,26 @@ public:
 	// Fx65 - LD Vx, [I]
 	void OP_Fx65();
 
+
 private:
 	std::default_random_engine randGen;
 	std::uniform_int_distribution<uint8_t> randByte;
+
+	void Table0();
+
+	void Table8();
+
+	void TableE();
+
+	void TableF();
+
+	void OP_NULL() {};
+
+	typedef void (Chip8::* Chip8Func)();
+	Chip8Func table[0xF + 1];
+	Chip8Func table0[0xE + 1];
+	Chip8Func table8[0xE + 1];
+	Chip8Func tableE[0xE + 1];
+	Chip8Func tableF[0x65 + 1];
+
 };
